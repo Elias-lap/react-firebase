0cd98158c1f248bf9d9468ef1fa6b120de71b2c7
Elias-zerz <Eliaszerz92@gmail.com>
2023-12-05 22:52:18 +0100

    ErrorPage

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,37 @@
 
-
+import './ErrorPage.css'
 function ErrorPage() {
   return (
-    <div className="error">
-      <h1>Errorr Pageee ..........................😎</h1>
+  <>
+  
+  <div className="stars">
+  <div className="custom-navbar">
+  </div>
+  <div className="central-body">
+    <img className="image-404" src="http://salehriaz.com/404Page/img/404.svg" width="300px" />
+  </div>
+  <div className="objects">
+    <img className="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px" />
+    <div className="earth-moon">
+      <img className="object_earth" src="http://salehriaz.com/404Page/img/earth.svg" width="100px" />
+      <img className="object_moon" src="http://salehriaz.com/404Page/img/moon.svg" width="80px" />
     </div>
+    <div className="box_astronaut">
+      <img className="object_astronaut" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px" />
+    </div>
+  </div>
+  <div className="glowing_stars">
+    <div className="star" />
+    <div className="star" />
+    <div className="star" />
+    <div className="star" />
+    <div className="star" />
+  </div>
+</div>
+
+  
+  
+  </>
   )
 }
 
